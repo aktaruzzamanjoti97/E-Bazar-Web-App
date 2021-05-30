23462d6d481e48b72f8d7e5a682611f8543aa50a
Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2021-05-30 13:30:13 +0600

    logo change

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,9 +13,9 @@ function Header() {
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
-            src="https://links.papareact.com/f90"
+            src="/logoMain1.png"
             width={150}
-            height={40}
+            height={45}
             objectFit="contain"
             className="cursor-pointer"
           />
@@ -55,20 +55,20 @@ function Header() {
       </div>
 
       {/* Bottom Nav */}
-      <div  className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
-          <p className="link flex items-center">
-              <MenuIcon className="h-6 mr-1"/>
-              All
-          </p>
-          <p className="link">Prime Video</p>
-          <p className="link">Amazon Business</p>
-          <p className="link">Today's Deals</p>
-          <p className="link hidden lg:inline-flex">Electronic</p>
-          <p className="link hidden lg:inline-flex">Food & Grocery</p>
-          <p className="link hidden lg:inline-flex">Prime</p>
-          <p className="link hidden lg:inline-flex">Buy Again</p>
-          <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
-          <p className="link hidden lg:inline-flex">Health & Personal Care</p>
+      <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+        <p className="link flex items-center">
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's Deals</p>
+        <p className="link hidden lg:inline-flex">Electronic</p>
+        <p className="link hidden lg:inline-flex">Food & Grocery</p>
+        <p className="link hidden lg:inline-flex">Prime</p>
+        <p className="link hidden lg:inline-flex">Buy Again</p>
+        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+        <p className="link hidden lg:inline-flex">Health & Personal Care</p>
       </div>
     </header>
   );
--- src/components/Product.js
+++ src/components/Product.js
@@ -2,7 +2,7 @@ import { StarIcon } from "@heroicons/react/solid";
 import Image from "next/image";
 import React from "react";
 import { useState } from "react";
-import Currency from 'react-currency-formatter';
+import Currency from "react-currency-formatter";
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
@@ -14,12 +14,14 @@ const Product = ({ id, title, price, description, category, image }) => {
   const [hasPrime] = useState(Math.random() < 0.5);
 
   return (
-    <div className='relative flex flex-col m-5 bg-white z-30 p-10'>
-      <p className='absolute top-2 right-2 text-xs italic text-gray-400'>{category}</p>
+    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+      <p className="absolute top-2 right-2 text-xs italic text-gray-400">
+        {category}
+      </p>
 
       <Image src={image} height={200} width={200} objectFit="contain" />
 
-      <h4 className='my-3'>{title}</h4>
+      <h4 className="my-3">{title}</h4>
 
       <div className="flex">
         {Array(rating)
@@ -29,7 +31,7 @@ const Product = ({ id, title, price, description, category, image }) => {
           ))}
       </div>
 
-      <p className='text-xs my-2 line-clamp-2'>{description}</p>
+      <p className="text-xs my-2 line-clamp-2">{description}</p>
 
       <div className="mb-5">
         <Currency quantity={price} currency="GBP" />
